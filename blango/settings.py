c478184d5ef33a07f3e45b5201971e9c5bf34e4c
Bartly Dyck <bartly.dyck@hotmail.ca>
2024-03-14 21:01:18 +0000

    Finish security and passwords

--- blango/settings.py
+++ blango/settings.py
@@ -63,6 +63,13 @@ class Dev(Configuration):
     },
 }
 
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
+
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
