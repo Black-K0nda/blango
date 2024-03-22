2de6117a27a27f63960c3826aedf242d7560f42d
Bartly Dyck <bartly.dyck@hotmail.ca>
2024-03-22 21:32:45 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -117,6 +117,9 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+        ],
+        "DEFAULT_PERMISSION_CLASSES": [
+          "rest_framework.permissions.IsAuthenticated",
         ]
     }
     MIDDLEWARE = [
