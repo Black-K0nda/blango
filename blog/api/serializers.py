912dc22c634486532d010e726c4594135e3b3093
Bartly Dyck <bartly.dyck@hotmail.ca>
2024-03-27 20:29:07 +0000

    Finish nested relationships

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -41,3 +41,22 @@ class PostSerializer(serializers.ModelSerializer):
     model = Post
     fields = "__all__"
     readonly = ['modified_at', 'created_at']
+
+
+class PostDetailSerializer(PostSerializer):
+  comments = CommentSerializer(many=True)
+
+  def update(self, instance, validated_data):
+    comments = validated_data.pop("comments")
+
+    instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+    for comment_data in comments:
+      if comment_data.get("id"):
+        continue
+      comment = Comment(**comment_data)
+      comment.creator = self.context['request'].user
+      comment.content_object = instance
+      comment.save()
+
+    return instance
