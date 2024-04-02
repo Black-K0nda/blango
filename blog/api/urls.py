609d12b373eddca85bd53d4cdd011c7e9a25a109
Bartly Dyck <bartly.dyck@hotmail.ca>
2024-04-02 19:31:24 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
-from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList, PostDetail, UserDetail
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 
+# Create a router and register the viewsets.
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register('posts', PostViewSet)
+# Define the schema view.
 schema_view = get_schema_view(
     openapi.Info(
         title="Blango API",
@@ -15,25 +20,14 @@ schema_view = get_schema_view(
     url=f"https://{os.environ.get('CODIO_HOSTNAME')}-8000.codio.io/api/v1/",
     public=True,
 )
+
+# Define the urlpatterns.
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
-    path('users/<str:email>', UserDetail.as_view(), name='api_user_detail'),
-]
-urlpatterns += [
+    # path("posts/", PostList.as_view(), name="api_post_list"),
+    # path("posts/<int:pk>/", PostDetail.as_view(), name="api_post_detail"),
+    path("users/<str:email>/", UserDetail.as_view(), name="api_user_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-    #     schema_view.without_ui(cache_timeout=0),
-    #     name="schema-json",
-    # ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("", include(router.urls)),
 ]
-
-
-
-urlpatterns = format_suffix_patterns(urlpatterns)
